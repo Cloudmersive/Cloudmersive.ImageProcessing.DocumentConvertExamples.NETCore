51a1a837753a94e29851142bf9212fdde6b03530
Cloudmersive <35204726+Cloudmersive@users.noreply.github.com>
2019-07-05 11:13:08 -0700

    Added

---
+++ Cloudmersive.Image.Docs.NETCore/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Cloudmersive.Image.Docs.NETCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
