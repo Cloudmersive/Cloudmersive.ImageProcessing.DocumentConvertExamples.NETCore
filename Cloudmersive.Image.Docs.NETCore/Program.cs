0eb95230be3f1927e5c19b5cd9711bb553dc1798
Cloudmersive <35204726+Cloudmersive@users.noreply.github.com>
2019-07-05 11:17:22 -0700

    Update Program.cs

--- Cloudmersive.Image.Docs.NETCore/Program.cs
+++ Cloudmersive.Image.Docs.NETCore/Program.cs
@@ -1,4 +1,11 @@
 ﻿using System;
+using System.Diagnostics;
+using Cloudmersive.APIClient.NETCore.DocumentAndDataConvert.Api;
+using Cloudmersive.APIClient.NETCore.DocumentAndDataConvert.Client;
+using Cloudmersive.APIClient.NETCore.DocumentAndDataConvert.Model;
+using Cloudmersive.APIClient.NETCore.ImageRecognition.Api;
+using Cloudmersive.APIClient.NETCore.ImageRecognition.Client;
+using Cloudmersive.APIClient.NETCore.ImageRecognition.Model;
 
 namespace Cloudmersive.Image.Docs.NETCore
 {
@@ -8,7 +15,43 @@ namespace Cloudmersive.Image.Docs.NETCore
         {
             // Resizing an image
 
+            Cloudmersive.APIClient.NETCore.ImageRecognition.Client.Configuration.Default.AddApiKey("Apikey", "YOUR_API_KEY");
+
+
+
+            var apiInstance1 = new ResizeApi();
+            var maxWidth = 56;  // int? | Maximum width of the output image - final image will be as large as possible while less than or equial to this width
+            var maxHeight = 56;  // int? | Maximum height of the output image - final image will be as large as possible while less than or equial to this height
+            var imageFile = new System.IO.FileStream("C:\\temp\\input.jpg", System.IO.FileMode.Open); // System.IO.Stream | Image file to perform the operation on.  Common file formats such as PNG, JPEG are supported.
+
+            try
+            {
+                // Resize an image with parameters
+                byte[] result = apiInstance1.ResizePost(maxWidth, maxHeight, imageFile);
+                Debug.WriteLine(result);
+            }
+            catch (Exception e)
+            {
+                Debug.Print("Exception when calling ResizeApi.ResizePost: " + e.Message);
+            }
+
             // Converting a Word Document (DOCX) to PDF
+
+            Cloudmersive.APIClient.NETCore.DocumentAndDataConvert.Client.Configuration.Default.AddApiKey("Apikey", "YOUR_API_KEY");
+
+            var apiInstance2 = new ConvertDocumentApi();
+            var inputFile = new System.IO.FileStream("C:\\temp\\input.docx", System.IO.FileMode.Open); // System.IO.Stream | Input file to perform the operation on.
+
+            try
+            {
+                // Word DOCX to PDF
+                byte[] result = apiInstance2.ConvertDocumentDocxToPdf(inputFile);
+                Debug.WriteLine(result);
+            }
+            catch (Exception e)
+            {
+                Debug.Print("Exception when calling ConvertDocumentApi.ConvertDocumentDocxToPdf: " + e.Message);
+            }
         }
     }
 }
